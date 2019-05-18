99abed11186fd52e2ad09b7c49680510b3762367
WurgBASH <just_my_mail@coolsite.net>
2019-05-19 00:11:15 +0300

    ver 0.0.1

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import time
 from flask import Flask, Response, redirect, request, url_for, jsonify,render_template
 
 text1 ='Nothing'
-
+app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def add_message():
 	global text1
 	if request.method == "POST":
-	    content = request.json
+		content = request.json
 		text1=content['mytext']
 		return render_template('hello.html', text=text1)
 	else:
