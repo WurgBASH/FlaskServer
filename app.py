3086b2fc2164c34a937185fcc29b90004685ffd2
WurgBASH <just_my_mail@coolsite.net>
2019-05-19 00:35:26 +0300

    3213

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import threading,os,sys,socket
 import time
 from flask import Flask, Response, redirect, request, url_for, jsonify,render_template
 
-users= {}
+users= [{}]
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def add_message():
 	if request.method == "POST":
 		content = request.json
-		users[content['first_name']] = content['message']
+		test[content['first_name']] = content['message']
+		users.append(test)
 		return render_template('index.html', users=users)
 	else:
 		return render_template('index.html', users=users)
