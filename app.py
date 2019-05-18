e97661d2bd39b7f2b3ab8dae8f71a11a2820785d
WurgBASH <just_my_mail@coolsite.net>
2019-05-19 00:22:53 +0300

    ver with 1 users dict

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import threading,os,sys,socket
 import time
 from flask import Flask, Response, redirect, request, url_for, jsonify,render_template
 
-text1 ='Nothing'
+users= {}
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def add_message():
-	global text1
 	if request.method == "POST":
 		content = request.json
-		text1=content['mytext']
-		return render_template('index.html', text=text1)
+		users[text2] = content['message']
+		return render_template('index.html', users=users)
 	else:
-		return render_template('index.html', text=text1)
+		return render_template('index.html', users=users)
 
 if __name__ == '__main__':
 	app.run()
