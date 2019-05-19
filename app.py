384b30db6193d76c9a50a580ab32142ed05bbed4
WurgBASH <just_my_mail@coolsite.net>
2019-05-19 23:20:59 +0300

    test1123

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 import threading,os,sys,socket
 import time
 from flask import Flask, Response, redirect, request, url_for, jsonify,render_template
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater,CallbackQueryHandler,RegexHandler,ConversationHandler
+import telegram
 
-users= []
+TOKEN = "710118383:AAFJuBvAtwZ4yWvkjdmBGL6pZb6ocP4e0S4"
+PORT = int(os.environ.get('PORT', '8443'))
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+text = 'testing'
+
+def handle_message1(bot, update):
+	global text
+	text =update.message.text
+
+
+
+dispatcher.add_handler(MessageHandler(Filters.text, handle_message1))
+
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def add_message():
-	if request.method == "POST":
-		content = request.json
-		users.append({content['first_name']:content['message']})
-		return render_template('index.html', users=users)
-	else:
-		return render_template('index.html', users=users)
+	global text
+	updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+	updater.bot.set_webhook("https://flaskappprogram.herokuapp.com/" + TOKEN)
+	updater.idle()
+
+	if request.headers.get('accept') == 'text/event-stream':
+		def events():
+			yield "data: %s\n\n" % (text)
+			time.sleep(.5)
+		return Response(events(), content_type='text/event-stream')
+	return redirect(url_for('static', filename='index.html'))
+
 
 if __name__ == '__main__':
 	app.run()
