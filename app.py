9cf22edf5469148ed168bef232eca0935f6bdaaf
WurgBASH <just_my_mail@coolsite.net>
2019-05-20 22:29:54 +0300

    last ver with message

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 import threading,os,sys,socket
 import time
 from flask import Flask, Response, redirect, request, url_for, jsonify,render_template, make_response
-from flask_socketio import SocketIO, emit, join_room, leave_room,close_room, rooms, disconnect,Namespace
 import telegram
 import pymongo
 import logging
 import json
-from threading import Lock
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
 
-msg_RESPONSE = 'ADESQ#$@#s'
-name_RESPONSE = 'None'
-data_base_response = 0 
-async_mode = None
-dex = False
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
-
-
 bot = telegram.Bot(token="710118383:AAFJuBvAtwZ4yWvkjdmBGL6pZb6ocP4e0S4")
-
 
 client = pymongo.MongoClient("mongodb+srv://wurg:wurg@pythonacademy-9pn3o.gcp.mongodb.net/test?retryWrites=true")
 db = client.get_database('PythonAcademy_db')
-
-
-
 
 
 @app.route('/')
@@ -35,16 +19,12 @@ def index():
 	return render_template('index.html')
 @app.route('/getJSONfromBot',methods=['POST'])
 def json_handle():
-	
-	print('SSSSSSSSS')
 	print('json_handle was started')
 	if request.method == 'POST':
 		content = request.get_json()
 		if content['message_text'] != None:
-			socketio.emit('my_response',
-                      {'text':content['message_text'] , 'user': content['user_name']},
-                      namespace='/test')
 			db.messages.insert_one({'user_id': content['user_id'], 'user_name':content['user_nick'], 'first_name':content['user_name'],'message_text':content['message_text']})
+
 @app.route('/send_message',methods=['POST'])
 def send_message():
 	if request.method == 'POST':
@@ -54,53 +34,28 @@ def send_message():
 
 @app.route('/messages',methods=['GET','POST'])
 def add_message():
-	#if request.method == 'GET':
-	return render_template('messages.html', async_mode=socketio.async_mode)
-	# else:
-	# 	data = {}
-	# 	ges = db.messages.count()
-	# 	res = db.messages.find()
-	# 	for x in res:
-
-	# 		nam=[]
-	# 		for key,val in x.items():
-	# 			if(key=='first_name'):
-	# 				nam.append(val)
-	# 				#data[val] = []
-	# 			elif(key == 'message_text'):
-	# 				nam.append(val)
-	# 				#data[val].append(val)
-	# 		if nam[0] in data:
-	# 			data[nam[0]].append(nam[1])
-	# 		else:
-	# 			data[nam[0]] = [nam[1]]
-	# 	print(data)
-	# 	return json.dumps(data)
-
-class MyNamespace(Namespace):
-	def on_my_event(self, message):
-		emit('connection',
-             {'data': 'sasa', 'count': 2})
-
-def background_thread():
-	count = 0
-	while True:
-		socketio.sleep(10)
-		count += 1
-		socketio.emit('connection',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    global thread
-    # with thread_lock:
-    #     if thread is None:
-    #         thread = socketio.start_background_task(background_thread)
-    emit('connection', {'data': 'Connected', 'count': 0})
-
-socketio.on_namespace(MyNamespace('/test'))
+	if request.method == 'GET':
+		return render_template('messages.html')
+	else:
+		data = {}
+		ges = db.messages.count()
+		res = db.messages.find()
+		for x in res:
+			nam=[]
+			for key,val in x.items():
+				if(key=='first_name'):
+					nam.append(val)
+					#data[val] = []
+				elif(key == 'message_text'):
+					nam.append(val)
+					#data[val].append(val)
+			if nam[0] in data:
+				data[nam[0]].append(nam[1])
+			else:
+				data[nam[0]] = [nam[1]]
+		print(data)
+		return json.dumps(data)
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True)
+	app.run()
 	
