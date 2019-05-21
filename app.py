ca0dd1ce03968d260b0dc4085452d714dc50c161
WurgBASH <just_my_mail@coolsite.net>
2019-05-21 20:21:45 +0300

    first test bot msg

--- app.py
+++ app.py
@@ -10,20 +10,25 @@ import json
 from flask_socketio import SocketIO,emit
 
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app, async_handlers=True)
 bot = telegram.Bot(token="710118383:AAFJuBvAtwZ4yWvkjdmBGL6pZb6ocP4e0S4")
 
+#---------------------------------------------
+
 client = pymongo.MongoClient("mongodb+srv://wurg:wurg@pythonacademy-9pn3o.gcp.mongodb.net/test?retryWrites=true")
 db = client.get_database('PythonAcademy_db')
 
-
+#---------------------------------------------
 
 def GetUserId(user_name):
 	res = db.messages.find({'user_name': user_name})
 	for x in res:
 		return x['user_id']
+
+#---------------------------------------------
 
 def GetAllUsers():
 	users={}
@@ -40,43 +45,76 @@ def GetAllUsers():
 			users[nam[0]] = nam[1]
 	return users
 
+#---------------------------------------------
+
 def GetAllMessage():
 	messsagesFrom = {}
 	res = db.messages.find()
-	dex=False
+	dex=True
 	for x in res:
 		nam=[]
 		for key,val in x.items():
-			if (key == 'user_name'):
+			if  (key == 'bot_bool'):
+				dex = False
+			elif (key == 'user_name'):
 				nam.append(val)
+			elif (key=='first_name'):
+				if dex:
+					nam.append(val)	
+				else:
+					nam.append('Бот')
+					dex=False
 			elif(key == 'message_text'):
 				nam.append(val)
-				
+
 		if nam[0] in messsagesFrom:
-			messsagesFrom[nam[0]].append(nam[1])
+			messsagesFrom[nam[0]].append({nam[1]:nam[2]})
 		else:
-			messsagesFrom[nam[0]] = [nam[1]]
+			messsagesFrom[nam[0]] = [{nam[1]:nam[2]}]
 	return messsagesFrom
+
+#---------------------------------------------
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+#---------------------------------------------
+
 @app.route('/getJSONfromBot',methods=['POST'])
 def json_handle():
 	print('json_handle was started')
 	if request.method == 'POST':
 		content = request.get_json()
 		if content['message_text'] != None:
+			db.messages.insert_one({'bot_bool': 'bot','user_id': content['user_id'], 'user_name':content['user_nick'], 'first_name':content['user_name'],'message_text':content['message_text']})
+			socketio.emit('bot_msg', {'user_name':content['user_name'],'message_text':content['message_text'],'user_nick':content['user_nick']},namespace='/test')
+	return 'okay'
+#---------------------------------------------
+
+@app.route('/getJSONBOTfromBot',methods=['POST'])
+def jsonbot_handle():
+	print('json_handle was started')
+	if request.method == 'POST':
+		content = request.get_json()
+		if content['message_text'] != None:
 			db.messages.insert_one({'user_id': content['user_id'], 'user_name':content['user_nick'], 'first_name':content['user_name'],'message_text':content['message_text']})
 			socketio.emit('my_response', {'user_name':content['user_name'],'message_text':content['message_text'],'user_nick':content['user_nick']},namespace='/test')
-	return 'okay'
+	return 'okay'	
+
+#---------------------------------------------
 
 @app.route('/messages',methods=['GET','POST'])
 def add_message():
 	return render_template('messages.html', async_mode=socketio.async_mode, users=GetAllUsers(), messages = GetAllMessage())
+
+#---------------------------------------------
+
 @socketio.on('connect', namespace='/test')
 def test_connect():
 	emit('connected', {'data': 'Connected'})
+
+#---------------------------------------------
 
 @socketio.on('send_message', namespace='/test')
 def sending(mes):
