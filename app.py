65f8d47bc80bcfd8b22c0a13efe7c09e49914960
WurgBASH <just_my_mail@coolsite.net>
2019-05-21 20:55:28 +0300

    5s

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ def sending(mes):
 	cht_id=GetUserId(mes['username'])
 	users = db.messages.find({'user_id':cht_id}).limit(1) 
 	user =users[0]
-	db.messages.insert_one({'user_id': user['user_id'], 'user_name':user['user_name'], 'first_name':user['user_name'],'message_text':mes['message_text']})
-	socketio.emit('bot_msg', {'user_name':user['user_name'],'message_text':mes['message_text'],'user_nick':user['user_nick']},namespace='/test')
+	db.messages.insert_one({'user_id': user['user_id'], 'user_name':user['user_name'], 'first_name':user['first_name'],'message_text':mes['message_text']})
+	socketio.emit('bot_msg', {'user_name':user['first_name'],'message_text':mes['message_text'],'user_nick':user['user_name']},namespace='/test')
 
 	bot.send_message(chat_id=cht_id, text=mes['message_text'])
 
