63b7ca35e21536ce755c5e006b3a58c2e89ad8c4
WurgBASH <just_my_mail@coolsite.net>
2019-06-22 11:29:00 +0300

    printing

--- app.py
+++ app.py
@@ -174,6 +174,7 @@ def test_connect():
 
 @socketio.on('send_message', namespace='/test')
 def sending(mes):
+	print(mes['user_id'])
 	users = db.messages.find({'user_id':mes['user_id']}).limit(1) 
 	user =users[0]
 	db.messages.insert_one({'user_id': user['user_id'], 'user_name':user['user_name'], 'first_name':user['first_name'],'message_text':mes['message_text']})
